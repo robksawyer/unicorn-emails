2dbb31de4f9a254028768d4a98cad801a0c2795f
Rob Sawyer <rsawyer@tripwire.com>
2017-05-08 11:29:52 -0700

    Basic updates

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require("fs");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,9 +10,52 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.json({
-    results: 'Welcome to unicorn emails!'
+// Read Synchrously
+var domains = fs.readFileSync("public/lists/domains.json");
+var names = fs.readFileSync("public/lists/names.json");
+
+// Define to JSON type
+var jsonDomains = JSON.parse(domains);
+var jsonNames = JSON.parse(names);
+
+app.get('/', function(req, res) {
+  // Default to 50 emails.
+  var total = 50;
+  res.json({
+    results: 'Test',
+    meta: {
+      requestedAt: Date.now()
+    }
+  });
+});
+
+app.get('/:total/?:format', function(req, res) {
+  var total = 50;
+  var format = 'json';
+  // Limit to 100 emails.
+  if(parseInt(req.params.total) > 100){
+    total = 100;
+  }
+
+  // Set the format
+  switch(req.params.format) {
+    case 'text':
+    case 'txt':
+      format = 'text';
+      break;
+
+    default:
+      format = 'json';
+      break;
+  }
+  res.json({
+    results: {
+      meta: {
+        total: total,
+        format: format,
+        requestedAt: Date.now()
+      }
+    }
   });
 });
 
